793b01a9e83c9308093e7d2d9597624e777bb75f
bibek977 <bhattaraibibek91@gmail.com>
2021-12-26 14:32:09 +0545

    remove the JsX fragment

--- src/App.js
+++ src/App.js
@@ -3,10 +3,6 @@ import './App.css';
 
 function App() {
   return (
-    <>
-      <h1>
-        Fragments in JsX
-      </h1>
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -23,7 +19,6 @@ function App() {
         </a>
       </header>
     </div>
-    </>
   );
 }
 
