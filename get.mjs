fe95330cc850c0a74a4059fae009ebbb14c95f01
bibek977 <bhattaraibibek91@gmail.com>
2021-12-30 22:45:31 +0545

    adding mjs

---
+++ get.mjs
@@ -0,0 +1,5 @@
+let fname = "Bibek";
+let lname = "Bhattarai";
+
+export default fname
+export {lname , fname}
---
+++ show.mjs
@@ -0,0 +1,5 @@
+import name , {lname , fname} from "./get.mjs"
+
+console.log(name)
+console.log(lname)
+console.log(lname + fname)
