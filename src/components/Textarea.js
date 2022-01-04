53636c0eaa061edf02d1bdb2ecf42af1d77c08ef
bibek977 <bhattaraibibek91@gmail.com>
2022-01-04 17:18:18 +0545

    some changes in textarea

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -2,9 +2,15 @@ import React,{useState} from 'react'
 import './css/textarea.css'
 
 export default function Textarea(props) {
-    const clickedButton = ()=>{
+    const toUpper = ()=>{
         // console.log("button is clicked "  + initText)
         let changedText = initText.toUpperCase()
+        finalText(changedText)
+    }
+
+    const toLower = ()=>{
+        // console.log("button is clicked "  + initText)
+        let changedText = initText.toLowerCase()
         finalText(changedText)
     }
     const onChangeTextarea = (event)=>{
@@ -12,11 +18,17 @@ export default function Textarea(props) {
         finalText(event.target.value)
     }
     const [initText,finalText]=useState("")
-    return (
+    return (<>
         <div className='textarea'>
             <label htmlFor="textTransform">{props.text}</label>
             <textarea  id ='textTransform' value={initText} onChange={onChangeTextarea}></textarea>
-            <button onClick={clickedButton}>Change</button>
+            <button onClick={toUpper}>Uppercase</button>
+            <button onClick={toLower}>Lowercase</button>
         </div>
+        <div className="result">
+            <p>{initText.split(" ").length} words </p> <p> {initText.length} letters </p> <p> times to read : {0.008 * initText.split(" ").length} minutes</p>
+        </div>
+        
+    </>
     )
 }
